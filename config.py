2e02e54a71794d2cddc448cb3bc47ff69f0ba6e8
Aditya Rastogi <98271720+AdityaR-Dev98@users.noreply.github.com>
2024-12-25 16:26:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-MONGODB_URI = "mongodb://localhost:27017"
-MODEL_PATH = "C:\\Users\\rasto\\Downloads\\SecureChromeBackend\\random_forest_pipeline.pkl"
-FIREBASE_CREDENTIALS_PATH = "C:\\Users\\rasto\\Downloads\\SecureChromeBackend\\keys\\chromeextension-ba7af-firebase-adminsdk.json"
+MONGODB_URI = "-"
+MODEL_PATH = "-"
+FIREBASE_CREDENTIALS_PATH = "-"
